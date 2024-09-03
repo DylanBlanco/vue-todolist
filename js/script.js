2d82c9e0c1a1c3112691494a189c9e5259eeee6d
DylanBlanco <dylanblanco1198@gmail.com>
2024-09-03 16:18:11 +0200

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -5,16 +5,57 @@ createApp(
         data() {
             return {
                 todosList: [
-                    'MI SVEGLIO',
-                    'FACCIO COLAZIONE',
-                    'SEGUO LA LEZIONE',
-                    'FACCIO ESERCIZIO DEL GIORNO ASSEGNATO',
-                    'VADO IN PALESTRA',
-                    'CENO',
-                    'FINISCO ESERCIZIO ASSEGNATO',
-                    'VADO A DORMIRE',
-                ]
+                    {
+                        text: 'MI SVEGLIO',
+                        done:true,
+                    },
+                    {
+                        text: 'FACCIO COLAZIONE',
+                        done:false,
+                    },
+                    {
+                        text: 'SEGUO LA LEZIONE',
+                        done:true,
+                    },
+                    {
+                        text: 'FACCIO ESERCIZIO DEL GIORNO ASSEGNATO',
+                        done:true,
+                    },
+                    {
+                        text: 'VADO IN PALESTRA',
+                        done:false,
+                    },
+                    {
+                        text: 'CENO',
+                        done:true,
+                    },
+                    {
+                        text: 'FINISCO ESERCIZIO ASSEGNATO',
+                        done:false,
+                    },
+                    {
+                        text: 'VADO A DORMIRE',
+                        done:true,
+                    },
+                ],
+                newTodo:''
             };
+        },
+        methods: {
+            addTodo() {
+                const cleanTodo = this.newTodo.trim();
+
+                if(cleanTodo != '') {
+                    this.todosList.push({
+                        text: cleanTodo,
+                        done: true,
+                    });
+                    this.newTodo = '';
+                }
+            },
+            removeTodo(i) {
+                this.todosList.splice(i, 1);
+            }
         }
     }
 ).mount('#app');
